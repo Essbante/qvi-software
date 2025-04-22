d07bcab4456c5604560f8068cf004aa8853b91cf
essbante <esteban.garcia@gmail.com>
2025-04-22 20:06:10 +0000

    feat: add and update jupyter notebooks

--- trainings/deploy.sh
+++ trainings/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Check if vlei network exists, create it if not
-echo "Checking for vlei network..."
-if ! docker network ls | grep -q vlei; then
-    echo "vlei network not found. Creating it..."
-    docker network create vlei
-else
-    echo "vlei network already exists."
-fi
+# echo "Checking for keri_network..."
+# if ! docker network ls | grep -q keri_network; then
+#     echo "keri_network not found. Creating it..."
+#     docker network create keri_network
+# else
+#     echo "keri_network already exists."
+# fi
 
 # Stop and remove existing containers, networks defined in the compose file
 echo "Stopping and removing existing containers..."
